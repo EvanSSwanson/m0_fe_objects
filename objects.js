ad22b3abc0ad170b09b93bde1884173c8924768f
Evan Swanson <evan.sundelius@gmail.com>
2022-08-04 12:46:02 -0500

    Initial commit

--- objects.js
+++ objects.js
@@ -6,31 +6,32 @@
 // file in your Terminal.
 
 foods = {
-  apples: 23, 
-  grapes: 507, 
-  eggs: 48
+  apples: 23,
+  grapes: 507,
+  eggs: 48,
+  tomatoes:18
 }
 
-// Write code that prints all of the 'keys' of the foods variable 
+// Write code that prints all of the 'keys' of the foods variable
 // you created above:
-// YOUR CODE HERE
+console.log(Object.keys(foods));
 
-// Write code that prints all of the 'values' of the foods variable 
+// Write code that prints all of the 'values' of the foods variable
 // you created above:
-// YOUR CODE HERE
+console.log(Object.values(foods));
 
-// Write code that prints the value of the second food of the foods variable 
+// Write code that prints the value of the second food of the foods variable
 // you created above:
-// YOUR CODE HERE
+console.log(Object.values(foods)[1])
 
-// Write code that adds a food to the foods object. 
+// Write code that adds a food to the foods object.
 // Then, print the updated object:
-// YOUR CODE HERE
+console.log(Object.keys(foods)[3])
 
 
-#-------------------
+//#-------------------
 // Part 2: Email
-#-------------------
+//#-------------------
 
 
 // Think about all the pieces of information associated with one single email in your inbox.
@@ -38,22 +39,29 @@ foods = {
 
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+var email1 = {
+  senderName: "Kai Swanson",
+  senderAddress: "notactuallykai@gmail.com",
+  subject: "Trip Itinerary",
+  timestamp: "11:20 AM, August 3, 2022",
+  body: "Hey bud, just checking in to see if you read through our trip itinerary. We'll talk soon!"
+}
 
 // Write code that logs your email object to the terminal.
+console.log(email1);
 
-
-// Write code that logs all of the 'keys' of the email object 
+// Write code that logs all of the 'keys' of the email object
 // you created above:
-// YOUR CODE HERE
+console.log(Object.keys(email1));
 
-// Write code that logs all of the 'values' of the email object 
+// Write code that logs all of the 'values' of the email object
 // you created above:
-// YOUR CODE HERE
+console.log(Object.values(email1));
 
 
-#-------------------
+//#-------------------
 // Part 3: Many Emails - CHALLENGE!
-#-------------------
+//#-------------------
 
 // LONG EXAMPLE:
 // Now that we've learned about Objects AND Arrays, we can combine them.
@@ -76,7 +84,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +98,36 @@ console.log(posts);
 console.log(posts[0]);
 
 
-// The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-// Objects. Each of those Objects has 4 key-value pairs. This may LOOK 
+// The code snippet above shows an Array with 2 elements. Each element in the Array is a
+// Objects. Each of those Objects has 4 key-value pairs. This may LOOK
 // a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 // good to have some exposure before going into Mod 1.
 
 
-// YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Objects, using the same
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+emails = [
+  {
+    senderName: "Kai Swanson",
+    senderAddress: "notactuallykai@gmail.com",
+    subject: "Trip Itinerary",
+    timestamp: "11:20 AM, August 3, 2022",
+    body: "Hey bud, just checking in to see if you read through our trip itinerary. We'll talk soon!"
+  },
+  {
+    senderName: "totally real energy company",
+    senderAddress: "yepweareforsurereal@gmail.com",
+    subject: "Your energy bills are overdue!",
+    timestamp: "10:23 PM, August 2, 2022",
+    body: "Hello, you owe us money and you can trust us but uhhh please don't look up our company just trust us its real"
+  },
+  {
+    senderName: "SmartyKat",
+    senderAddress: "smartykat.outreach@gmail.com",
+    subject: "Regarding you improper use of our product",
+    timestamp: "9:48 AM, August 2, 2022",
+    body: "Mr. Swanson, we apologize, but our packaging clearly states our cat nip is not for human consumption. There is nothing we can do."
+  }
+];
+console.log(emails);
--- selfEval.md
+++ selfEval.md
@@ -1,5 +1,13 @@
 # Self Evaluation
 
 - What did you struggle with in this section?
+
+Thankfully, this lesson went quite smoothly. I feel confident with the content covered in this lesson.
+
 - How is note-taking going? What are you finding helpful? When do you find yourself taking the most helpful notes?
+
+I tend to slack on my note-taking. To remedy that, I've gotten into the habit of going back over my lessons at the end of the day to make proper notes then. It also serves as a nice "refresher course."
+
 - What are you noticing that you lean into - Googling, or trying to solve a problem in code on your own first?
+
+I'd say both options are ones I employ often; it truthfully depends on the code I'm working on. I typically get as far as I can manage simply experimenting with code first, then go to Google when I've exhausted my options.
